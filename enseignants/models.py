7f056c0d581e1aae8a6766129a99397ef0d56699
cyprienbf <contact.cyprien@pm.me>
2025-06-14 12:27:02 +0200

    Global Update

--- enseignants/models.py
+++ enseignants/models.py
@@ -1,17 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-# On étend le modèle User de Django avec un profil Enseignant
 class Enseignant(models.Model):
-    # Relation un-à-un avec le User. Si le User est supprimé, le profil Enseignant aussi.
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # nom et prenom sont déjà dans le modèle User, mais on peut les redoubler ici si on veut
-    # ou simplement utiliser user.first_name et user.last_name
-    
+    nom = models.CharField(max_length=100)
+    prenom = models.CharField(max_length=100)
+
     def __str__(self):
-        # Utilise le prénom et le nom du modèle User associé
-        return f"{self.user.first_name} {self.user.last_name}"
+        return f"{self.prenom} {self.nom}"
 
     class Meta:
         verbose_name = "Enseignant"
         verbose_name_plural = "Enseignants"
+        ordering = ['nom', 'prenom']
--- enseignants/views.py
+++ enseignants/views.py
@@ -1,6 +1,5 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.models import User
 from .models import Enseignant
 
 class EnseignantListView(ListView):
@@ -12,26 +11,15 @@ class EnseignantDetailView(DetailView):
     model = Enseignant
     template_name = 'enseignants/enseignant_detail.html'
 
-# Note: Une vue de création plus robuste gérerait la création de l'objet User associé.
-# Pour l'instant, nous supposons que le User est créé via l'admin ou un autre process.
-# Ceci est une simplification pour ce CRUD. La version complète nécessiterait un formulaire personnalisé.
-# Nous allons donc créer un formulaire simple qui demande de sélectionner un User existant.
 class EnseignantCreateView(CreateView):
     model = Enseignant
-    fields = ['user'] # On sélectionne un User existant non lié
+    fields = ['nom', 'prenom'] 
     template_name = 'enseignants/enseignant_form.html'
     success_url = reverse_lazy('enseignants:enseignant_list')
 
-    def get_form(self, form_class=None):
-        form = super().get_form(form_class)
-        # On filtre pour n'afficher que les utilisateurs qui ne sont pas déjà des enseignants
-        linked_users_pk = Enseignant.objects.all().values_list('user__pk', flat=True)
-        form.fields['user'].queryset = User.objects.exclude(pk__in=linked_users_pk)
-        return form
-
 class EnseignantUpdateView(UpdateView):
     model = Enseignant
-    fields = ['user']
+    fields = ['nom', 'prenom']
     template_name = 'enseignants/enseignant_form.html'
     success_url = reverse_lazy('enseignants:enseignant_list')
 
@@ -39,10 +27,3 @@ class EnseignantDeleteView(DeleteView):
     model = Enseignant
     template_name = 'enseignants/enseignant_confirm_delete.html'
     success_url = reverse_lazy('enseignants:enseignant_list')
-
-    # Il faut aussi supprimer le User associé pour ne pas le laisser "orphelin"
-    def form_valid(self, form):
-        user = self.get_object().user
-        response = super().form_valid(form)
-        user.delete()
-        return response
